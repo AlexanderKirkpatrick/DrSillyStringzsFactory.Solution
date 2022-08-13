c75ea16f2c74f75bef67980c8ea77d6a0b0d08c7
Alexander <knibz@protonmail.com>
2022-08-13 12:40:06 -0400

    add initial directories and files for MVC and CRUD structure, allowing for mySQL DB interaction

---
+++ Silly_Factory/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Silly_Factory.Models;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Silly_Factory.Controllers
+{
+    public class HomeController : Controller
+    {
+
+      [HttpGet("/")]
+      public ActionResult Index()
+      {
+        return View();
+      }
+    }
+}
